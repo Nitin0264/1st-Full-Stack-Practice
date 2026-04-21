1111d21cc0b82e5025d069e5b34a188a83acd0fb
Nitin0264 <nc8224140@gmail.com>
2026-04-21 20:53:27 +0530

    connecting the login to the backend is complete

--- Back-End/index.js
+++ Back-End/index.js
@@ -1,9 +1,10 @@
 import express from "express";
-
+import cors from "cors";
 const  app = express();
 
 app.set("view engine", "ejs");
-
+app.use(cors());
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/",(req,res)=>{
@@ -23,8 +24,8 @@ app.get("/login", (req, res) => {
 });
 
 app.post("/login", (req, res) => {
-  console.log(req.body);
-  res.send("Login data received");
+// const {email,pwd} = req.body;
+console.log(req.body)
 });
 
 app.get("/about",(req,res)=>
--- Front-End/src/Components/Login.jsx
+++ Front-End/src/Components/Login.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import axios from "axios";
 function Login() {
+  let [email, setEmail] = useState('')
+  let [pwd, setPwd] = useState('')
+  const onSubmitForm = async (event) => {
+    event.preventDefault();
+    let data = await axios.post("http://localhost:8000/login", { email, pwd })
+    console.log(data)
+  }
   return (
-     <div className='flex flex-col gap-9 justify-center items-center h-screen'>
-      
+    <div className='flex flex-col gap-9 justify-center items-center h-screen '>
+
       <h1 className='text-3xl text-center'>Full-Stack</h1>
 
       <div className='flex flex-col gap-6 items-center justify-center text-center'>
@@ -11,25 +18,30 @@ function Login() {
         <p className='text-xl'>or choose another way to sign in</p>
       </div>
 
-      <div className='main-section flex flex-col gap-5 items-center justify-center'>
-        <input 
-          className="border px-3 py-2 w-[400px]" 
-          type="email" 
-          placeholder='Nitin@gmail.com' 
-          name='email' 
-        />
 
-        <input 
-          className="border px-3 py-2 w-[400px]" 
-          type="password"  
-          placeholder='1234' 
-          name='pwd'
-        />
 
-        <button className='h-[35px] w-[400px] bg-black text-white'>
-          Sign IN
-        </button>
-      </div>
+      <form action="" onSubmit={onSubmitForm}>
+
+        <div className='main-section flex flex-col gap-5 items-center justify-center'>
+          <input onChange={(e) => setEmail(e.target.value)}
+            className="border px-3 py-2 w-[400px]"
+            type="email"
+            placeholder='Nitin@gmail.com'
+            name='email'
+          />
+
+          <input onChange={(e) => setPwd(e.target.value)}
+            className="border px-3 py-2 w-[400px]"
+            type="password"
+            placeholder='1234'
+            name='pwd'
+          />
+
+          <button className='h-[35px] w-[400px] bg-black text-white'>
+            Sign IN
+          </button>
+        </div>
+      </form>
 
     </div>
   )
