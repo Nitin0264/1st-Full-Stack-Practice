00357407622d67ff531e9c174c14fcaadb277223
Nitin0264 <nc8224140@gmail.com>
2026-05-03 21:51:44 +0530

    welcome to the home section

--- Front-End/src/Components/Home.jsx
+++ Front-End/src/Components/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Home() {
   return (
-    <div className='bg-red-300 w-full h-screen'>Home </div>
+    <div className='bg-red-300 w-full h-screen'>hello there this is a home section</div>
   )
 }
 
